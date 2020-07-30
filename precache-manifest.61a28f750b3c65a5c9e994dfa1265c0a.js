de186ce8c373e9e8678b2a91c0e1f0902724bcbf
wywang <727936199@qq.com>
2020-07-30 18:52:09 +0800

    init

---
+++ precache-manifest.61a28f750b3c65a5c9e994dfa1265c0a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "76e3d7101a9835fa3e2f5367d6a892bc",
+    "url": "/tallybook/index.html"
+  },
+  {
+    "revision": "5858b59fa22095999895",
+    "url": "/tallybook/static/css/main.c0216dd2.chunk.css"
+  },
+  {
+    "revision": "e956aadecd79343296f2",
+    "url": "/tallybook/static/js/2.32b6f112.chunk.js"
+  },
+  {
+    "revision": "99406820fc9e02f862dbd66e126ca114",
+    "url": "/tallybook/static/js/2.32b6f112.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5858b59fa22095999895",
+    "url": "/tallybook/static/js/main.3c696dae.chunk.js"
+  },
+  {
+    "revision": "92e376cc0e6cbe927c5c",
+    "url": "/tallybook/static/js/runtime-main.5bd5bacb.js"
+  }
+]);
